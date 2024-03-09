6386f55efbca5bd4dd8fc2e5b1db24d383c49a91
unknown <szergej911@gmail.com>
2024-03-09 11:39:39 +0100

    Setting up the main componenets and the OGP

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./public/Logo .svg" alt="Little Lemon Logo" />
+      <Nav />
+    </header>
+  );
+}
